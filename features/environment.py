cf848a045360299b1a89ce23f352412d1b7c5889
shivam <shivams@lambdatest.com>
2019-02-19 14:17:48 +0530

    code commenting

--- features/environment.py
+++ features/environment.py
@@ -37,10 +37,13 @@ def setup_desired_cap(desired_cap):
     :return:
     """
     if os.environ['env'] == 'jenkins':
+        #sets the platform type
         desired_cap["platform"] = desired_cap["operatingSystem"]
         del desired_cap["operatingSystem"]
+        #sets the browser version
         desired_cap["version"] = desired_cap["browserVersion"]
         del desired_cap["browserVersion"]
+        #sets the tunnel name if configured
         if "LT_TUNNEL_NAME" in os.environ:
             desired_cap["TunnelName"] = os.environ["LT_TUNNEL_NAME"]
     if "tunnel" in desired_cap:
@@ -48,21 +51,25 @@ def setup_desired_cap(desired_cap):
             desired_cap["tunnel"] = True
         elif desired_cap["tunnel"].lower() == "false":
             desired_cap["tunnel"] = False
+    #shows the console logs in LT dashboard if set to true
     if "console" in desired_cap:
         if desired_cap["console"].lower() == "true":
             desired_cap["console"] = True
         elif desired_cap["console"].lower() == "false":
             desired_cap["console"] = False
+    #shows the network logs in LT dashboard if set to true
     if "network" in desired_cap:
         if desired_cap["network"].lower() == "true":
             desired_cap["network"] = True
         elif desired_cap["network"].lower() == "false":
             desired_cap["network"] = False
+    #shows the network logs in LT dashboard if set to true
     if "visual" in desired_cap:
         if desired_cap["visual"].lower() == "true":
             desired_cap["visual"] = True
         elif desired_cap["visual"].lower() == "false":
             desired_cap["visual"] = False
+    # shows the video logs in LT dashboard if set to true
     if "video" in desired_cap:
         if desired_cap["video"].lower() == "true":
             desired_cap["video"] = True
